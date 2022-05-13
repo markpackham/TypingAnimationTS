986d75d488dae92a8c58834a27d20242f634c87a
mark <markpackham1@gmail.com>
2022-05-13 10:16:18 +0100

    log out to console our variables
    https://youtu.be/gWD0EJgBlYQ?t=725

--- src/Typewriter.ts
+++ src/Typewriter.ts
@@ -24,7 +24,8 @@ export default class Typewriter {
 
   typeString(string: string) {
     this.#queue.push(()=>{
-      return new Promise((resolve, reject) =>{
+      return new Promise((resolve) =>{
+        console.log(string)
         resolve()
       })
     })
@@ -32,21 +33,24 @@ export default class Typewriter {
   }
 
   deleteChars(number: number){
+    console.log(number)
     return this
   }
 
   // We use the default delete speed of the constructor if nothing is passed in
   deleteAll(deletingSpeed = this.deletingSpeed){
+    console.log(deletingSpeed)
     return this
   }
 
   pauseFor(duration: number){
+    console.log(duration)
     return this
   }
 
 async start() {
-  for(let cb of this.#queue){
-    await cb()
+  for(let callBack of this.#queue){
+    await callBack()
   }
   return this
 }
