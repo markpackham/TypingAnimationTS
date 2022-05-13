da778ec55100386233e608355f74db588d28595e
mark <markpackham1@gmail.com>
2022-05-13 08:17:28 +0100

    define types used in Typewriter
    https://youtu.be/gWD0EJgBlYQ?t=258

--- src/Typewriter.ts
+++ src/Typewriter.ts
@@ -1,6 +1,15 @@
 export default class Typewriter {
+  element: HTMLElement;
+  loop: boolean;
+  typingSpeed: number;
+  deletingSpeed: number;
   constructor(
     element: HTMLElement,
     { loop = false, typingSpeed = 50, deletingSpeed = 50 } = {}
-  ) {}
+  ) {
+    this.element = element;
+    this.loop = loop;
+    this.typingSpeed = typingSpeed;
+    this.deletingSpeed = deletingSpeed;
+  }
 }
